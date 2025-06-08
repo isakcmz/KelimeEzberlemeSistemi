a99015f865ccae3b527d8317825d9944251c5ca3
isakcmz <isafurkankacmaz@gmail.com>
2025-06-08 21:36:45 +0300

    Tasarım değişikliği yapıldı.
    a

--- FormSifreSifirla.Designer.cs
+++ FormSifreSifirla.Designer.cs
@@ -108,7 +108,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.Color.RosyBrown;
+            this.BackColor = System.Drawing.Color.IndianRed;
             this.ClientSize = new System.Drawing.Size(421, 353);
             this.Controls.Add(this.btnSifreDegistir);
             this.Controls.Add(this.txtYeniSifreTekrar);
